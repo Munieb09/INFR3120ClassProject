a7c69d8dd9bb85c40ec70d09dfc8d797a0fb3c45
Munieb09 <ahmed.sheikh@ontariotechu.net>
2022-11-18 15:09:39 -0500

    Cloud Deployment

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/bookStore"
+//    "URI": "mongodb://localhost/bookStore"
+    "URI":"mongodb+srv://Ahmed1:lpVQ7AvcNeKODNlB@mongodbserver.hsq1oaw.mongodb.net/BookStore"
 }
